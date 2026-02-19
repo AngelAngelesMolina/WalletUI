095ad45fd2998acbc3f02c6697cacabe96e3e50a
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-02-18 20:43:40 -0600

    ADD: SpendingItem

---
+++ app/src/main/java/com/jaamcoding/walletui/domain/models/SpendingItem.kt
@@ -0,0 +1,13 @@
+package com.jaamcoding.walletui.domain.models
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+
+
+data class SpendingItem(
+    val name:String,
+    val amount:Float,
+    val color : Color,
+    val icon : ImageVector
+)
+
